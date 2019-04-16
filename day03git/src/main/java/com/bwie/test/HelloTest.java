64e3af646857d9c69aacd4830f33534cc81849af
mahaowei <850378631@qq.com>
2019-04-16 12:06:24 +0800

    初始化

---
+++ day03git/src/main/java/com/bwie/test/HelloTest.java
@@ -0,0 +1,13 @@
+package com.bwie.test;
+
+/**
+ * @作者:马豪伟NB
+ * @时间:2019年4月16日
+ */
+public class HelloTest {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World.....");
+	}
+	
+}
